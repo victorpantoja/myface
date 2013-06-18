e1f802cdbb46f533695a813f1503b1d96ff0b192
Victor Pantoja <victor.pantoja@gmail.com>
2013-06-18 11:34:42 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'victorpantoja-myface'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures victorpantoja-myface'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: victorpantoja-myface
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'apache2'
+ 
+service 'apache2' do
+  action [:enable, :start]
+end
